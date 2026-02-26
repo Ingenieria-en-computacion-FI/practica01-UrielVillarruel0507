ee196437ddb056296f501636d1a2d4d431700a21
UrielVillarruel0507 <villarrueldavid43@gmail.com>
2026-02-26 01:33:53 -0600

    Update ej12.c

--- ejercicios/ej12.c
+++ ejercicios/ej12.c
@@ -1,6 +1,8 @@
 
-#include <stdio.h>
-int main(){
-    // Ejercicio 12
-    return 0;
-}
+Dir(m[i][j])=4000+((i×5)+j)×4
+Calculando:
+m[2][4] = 4056 (403A en hexadecimal)
+
+m[1][3] = 4032 (4020 en hexadecimal)
+
+4056−4032=24 bytes
