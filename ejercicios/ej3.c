5f9577b7c14367f737696843abd045d1b9b8570a
UrielVillarruel0507 <villarrueldavid43@gmail.com>
2026-02-26 01:08:51 -0600

    Update ej3.c

--- ejercicios/ej3.c
+++ ejercicios/ej3.c
@@ -1,6 +1,27 @@
 
 #include <stdio.h>
-int main(){
-    // Ejercicio 3
+
+int main() {
+
+    int a[3];
+    int b[3];
+    int c[3];
+    int i;
+
+    printf("Arreglo a:\n");
+    for(i = 0; i < 3; i++) {
+        printf("%p\n", &a[i]);
+    }
+
+    printf("\nArreglo b:\n");
+    for(i = 0; i < 3; i++) {
+        printf("%p\n", &b[i]);
+    }
+
+    printf("\nArreglo c:\n");
+    for(i = 0; i < 3; i++) {
+        printf("%p\n", &c[i]);
+    }
+
     return 0;
 }
